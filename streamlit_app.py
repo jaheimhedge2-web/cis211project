0725dad53a2167c93a610511ee408f3ee11b5993
jaheimhedge2-web <jaheimhedge2@icloud.com>
2025-11-03 11:07:25 -0500

    paste contents

---
+++ streamlit_app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd 
+from datetime import datetime
+
+# Page Config
+st.set_page_config(
+  page_title ='Avinash Jairam | Portfolio',
+  page_icon='🎯',
+  layout = 'wide'
+)
+
+# Custom CSS (optional - for styling)
+st.markdown('''
+                <style>
+                    .main-header {font-size: 42px; font-weight: bold; text-align:center;}
+                    .sub-header {font-size: 24px; text-align:center; color: #666;}
+                </style>
+            ''', unsafe_allow_html = True)
+
+# Sidebar
+st.sidebar.title('📍 Navigation')
+page = st.sidebar.radio('Go to',
+                        ['🏠 Home', '🤠 About', ' 💼 Projects', '🛠 Skills' ,'📝 Resume', '📩 Contact' ])
+
+# Home Page
+if page == '🏠 Home':
+  st.markdown('<p class="main-header">Avinash Jairam</p>', unsafe_allow_html=True)
+  st.markdown('<p class="sub-header">Aspiring Tech Professional | Medgar Evers College</p>', unsafe_allow_html=True)
