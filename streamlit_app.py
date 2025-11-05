317cf36f09f2bcf7386f14bb933830b0052665f3
jaheimhedge2-web <jaheimhedge2@icloud.com>
2025-11-05 12:16:27 -0500

    Update streamlit_app.py
    change name

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,5 +24,5 @@ page = st.sidebar.radio('Go to',
 
 # Home Page
 if page == '🏠 Home':
-  st.markdown('<p class="main-header">Avinash Jairam</p>', unsafe_allow_html=True)
+  st.markdown('<p class="main-header">Jaheim Hedge</p>', unsafe_allow_html=True)
   st.markdown('<p class="sub-header">Aspiring Tech Professional | Medgar Evers College</p>', unsafe_allow_html=True)
