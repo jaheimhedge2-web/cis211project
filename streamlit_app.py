f4e7bd3c1c378cebf6fd0f7559908806e67e67ef
jaheimhedge2-web <jaheimhedge2@icloud.com>
2025-11-05 12:31:56 -0500

    Update streamlit_app.py
    copied from 30

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,67 @@ page = st.sidebar.radio('Go to',
                         ['🏠 Home', '🤠 About', ' 💼 Projects', '🛠 Skills' ,'📝 Resume', '📩 Contact' ])
 
 # Home Page
-if page == '🏠 Home':
+if page == '🏠 Home': 
   st.markdown('<p class="main-header">Jaheim Hedge</p>', unsafe_allow_html=True)
   st.markdown('<p class="sub-header">Aspiring Business owner l | Medgar Evers College</p>', unsafe_allow_html=True)
+
+# Three Columns for stats
+  col1, col2, col3 = st.columns(3)
+
+  with col1:
+      st.metric('GPA', '3.8', '📚')
+  with col2:
+      st.metric('Projects', '5', '💻')
+  with col3:
+      st.metric('Skills', '10+', '🚀')
+
+  st.write('---')
+
+  # Introduction with columns
+  col1, col2 = st.columns([2,1])
+  with col1:
+    st.subheader('Welcome to my digital space!👋')
+    st.write('''
+                I am a Computer Information Systems student passionate about web development and emerging technologies. Currently learning
+                HTML, CSS, JavaScript, and Python to build innovative solutions.
+            
+                🎯 **Current Focus:** Building interactive web applications with Streamlit
+            
+                📚 **Currently Learning:** Internet and Emergin Technologies (CIS 211)
+            
+                🌱 **Fun Fact:** I can solve a Rubik's cube in under 2 minutes!
+            ''')
+  with col2:
+    # Placeholder for image
+    st.image('https://raw.githubusercontent.com/avinashjairam/cis211_project1/refs/heads/main/grumpy_cat.jfif', use_column_width=True)
+
+# About Page
+elif page == '🤠 About':
+  st.title('About Me')
+
+  # Timeline of my Professional Journey
+  st.subheader('My Journey 🗺️')
+
+  with st.expander('2025 - Present: Medgar Evers College'):
+    st.write('''
+                - Major: Computer Information Systems
+                - Relevant Coursework: Internet & Emerging Technologies, Programming, Database Systems, AI
+                - Activities: Track Team, Volleyball Team, Hackathon participant
+            ''')
+
+  with st.expander('2023 - 2025: NYC Museum School'):
+    st.write('''
+                - Graduated with honors
+                - AP Computer Science A (Score: 5)
+                - Founded Coding Club
+            ''')
+
+  st.subheader('Interests & Hobbies 🏀')
+  interests = ['Web Development', 'AI/Machine Learning', 'Photography', 'Basketball', 'Travel', 'Baseball']
+
+  # Display the interests in columns
+  cols = st.columns(3)
+  for i, interest in enumerate(interests):
+    with cols[i % 3]:
+      st.info(f'🔷 {interest}')
+  
