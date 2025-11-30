a7252f2efa78576a992fca6327b0a233cfffb1d3
jaheimhedge2-web <jaheimhedge2@icloud.com>
2025-11-30 16:34:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,8 +76,8 @@ elif page == '🤠 About':
     st.write('''
                 - Played on The Football Team
                 - Participated In Carribean Fest
+
                  -Spent my Sophmore year In Jamaica due to Covid-19
-               
             ''')
 
   st.subheader('Interests & Hobbies 🏀')
