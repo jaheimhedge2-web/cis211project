8c1e834f6a12eaf9ab478dbeb1f8bb8808a1ba04
jaheimhedge2-web <jaheimhedge2@icloud.com>
2025-12-01 11:18:22 -0500

    Removed blank space from projcts in navbar

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ st.markdown('''
 # Sidebar
 st.sidebar.title('📍 Navigation')
 page = st.sidebar.radio('Go to',
-                        ['🏠 Home', '🤠 About', ' 💼 Projects', '🛠 Skills' ,'📝 Resume', '📩 Contact' ])
+                        ['🏠 Home', '🤠 About', '💼 Projects', '🛠 Skills' ,'📝 Resume', '📩 Contact' ])
 
 # Home Page
 if page == '🏠 Home': 
